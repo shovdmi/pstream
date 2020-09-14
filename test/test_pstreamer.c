392a543c0f7f2126316478936f81bd6acbd96c1b
Dmitry <shovdmi@yandex.ru>
2020-09-14 13:44:43 +0300

    Added test for On Error situation

--- test/test_pstreamer.c
+++ test/test_pstreamer.c
@@ -35,3 +35,27 @@ void test_pstreamer_transmit_over_uart_on_no_errors(void)
 	TEST_ASSERT_EQUAL_INT(TRANSMIT_OK, result);
 }
 
+void test_pstreamer_transmit_over_uart_if_Out_Of_Memory_error_happend(void)
+{
+
+       uint8_t test_data[16];
+
+       mutex_lock_ExpectAndReturn(0, 0);
+
+       usart_transmit_ExpectAndReturn(0x7E, TRANSMIT_OK);
+
+       for (int i = 0; i < sizeof(test_data) ;i++)
+       {
+               test_data[i] = i;
+               usart_transmit_ExpectAndReturn(test_data[i], TRANSMIT_OK);
+       }
+
+       usart_transmit_ExpectAndReturn(0x7E, OUT_OF_MEMORY);
+
+       mutex_unlock_ExpectAndReturn(0, 0);
+       
+
+       int result = transmit_data(&test_data[0], sizeof(test_data));
+       TEST_ASSERT_EQUAL_INT(OUT_OF_MEMORY, result);
+}
+
